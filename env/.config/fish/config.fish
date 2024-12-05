e55f2aac32dbfbb682b1f91c112c7ec2c937ba8a
Lucas Fellipe Carvalho Moreira <lucas.fellipe.c@gmail.com>
2024-12-05 16:41:48 -0300

    initial commit

---
+++ env/.config/fish/config.fish
@@ -0,0 +1,24 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Aliases
+alias ifactive="ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'"
+alias copy="tr -d '\n' | pbcopy"
+alias vim="nvim"
+alias commit_message="git diff --cached | cody chat --stdin -m 'Write a commit message for this diff, do not use capital letters and use the angular conventional commits.'"
+
+# tmux
+set tmux (which tmux)
+alias tmux="tmux -2"
+
+# go
+set --export GOPATH "$HOME/go"
+
+function fish_right_prompt
+  # intentionally left blank
+end
+
+export PATH="$PATH:$HOME/.local/bin"
+export XDG_CONFIG_HOME="$HOME/.config"
+export DEV_ENV_HOME="$HOME/www/personal/dev-env"
